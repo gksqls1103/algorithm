8c3bca579b043189eb0a7646aea1674cc185818f
gksqls1103 <gksqls1103@naver.com>
2026-08-26 00:05:44 +0900

    [level 2] Title: 올바른 괄호, Time: 0.13 ms, Memory: 4.78 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    int count = 0;
+    
+    for (char c : s)
+    {
+        if (c == '(')
+        {
+            count++; 
+        }
+        else
+        {
+            count--; 
+            
+            
+            if (count < 0) 
+            {
+                return false;
+            }
+        }
+    }
+    
+    
+    return count == 0;
+}
